0b80307a95585c233aa956b66f2e1c464b7f6b5c
andreeaelenasamur <andelenasamur@gmail.com>
2025-03-31 11:02:19 +0200

    Store the firebase user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,40 @@
-import { inject, Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Injectable } from '@angular/core';
+import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, Unsubscribe } from '@angular/fire/auth';
 import { map, Observable } from 'rxjs';
 import { Usuario } from '../models/usuario.model';
 import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import * as actions from '../auth/auth.actions';
+import { getDocs, query, where } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  userUnsubscribe!: Unsubscribe;
+
   constructor(
     private auth: Auth,
-    private firestore: Firestore
+    private firestore: Firestore,
+    private store: Store<AppState>
   ) {}
 
-
   initAuthListener() {
-    return this.auth.beforeAuthStateChanged(user => {
-      console.log(user?.email);
-      console.log(user?.uid);
+    authState(this.auth).subscribe( async fUser => {
+      console.log(fUser)
+      if( fUser ) {
+        const userRef = collection(this.firestore, 'user');
+        const q = query(userRef, where("uid", "==", fUser.uid));
+        const querySnapshot = (await getDocs(q));
+        querySnapshot.forEach(( doc: any ) => {
+          this.store.dispatch(actions.setUser({ user: doc.data() }));
+        });
+      } else {
+        this.userUnsubscribe ? this.userUnsubscribe() : null;
+        this.store.dispatch(actions.unSetUser());
+      }
     })
   }
 
